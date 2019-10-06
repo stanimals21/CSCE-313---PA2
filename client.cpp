b7df1ec519b4e770f7c686cccf3851395bf03ba7
Shawn <shawntan2008@gmail.com>
2019-10-06 12:46:49 -0500

    test case for part 3 and closing channel statement added

--- client.cpp
+++ client.cpp
@@ -62,61 +62,92 @@ int main(int argc, char *argv[]){
 
 
 
-    // ----------------- part 2 ----------------- (change it up)
+    // ----------------- part 2 ----------------- 
+
+    // string fileName = "1.csv";
+    // const char* fileString = fileName.c_str();
+
+    // // prepare request array to be sent to server for FileSize
+    // char* requestArr = new char[sizeof(filemsg) + fileName.length() + 1];
+
+    // filemsg* msg = new filemsg(0, 0);
+
+    // memcpy(requestArr, msg, sizeof(filemsg));
+    // strcpy(requestArr + sizeof(filemsg), fileString);
+
+    // chan.cwrite(requestArr, sizeof(filemsg) + fileName.length() + 1);
+
+    // char* response = chan.cread();
+
+    // // Get packets of size 256MB and print to .csv file
+
+    // freopen("./received/y1.csv", "w", stdout); //  prompt to write to new .csv file
+
+    // __int64_t size = *(__int64_t*)response;
 
 
-    string fileName = "1.csv";
-    const char* fileString = fileName.c_str();
+    // // find number of requests to make
+    // int packetSize = 255;
+    // int iters = size / packetSize;
 
-    // prepare request array to be sent to server for FileSize
-    char* requestArr = new char[sizeof(filemsg) + fileName.length() + 1];
+    // // request multiple packets
+    // int i;
+    // for(i = 0; i <= iters; i++)
+    // {
+    //     if(size % packetSize !=0 && i == iters)
+    //     {
+    //         msg = new filemsg(i*packetSize, size % packetSize);
+    //     }
+    //     else
+    //     {
+    //         msg = new filemsg(i*packetSize, packetSize);
+    //     }
+    //     memcpy(requestArr, msg, sizeof(filemsg)); // replaces msg with new one
+    //     chan.cwrite(requestArr, sizeof(filemsg) + fileName.length() + 1);
+    //     response = chan.cread();
+    //     cout << response;
+    // }
 
-    filemsg* msg = new filemsg(0, 0);
-
-    memcpy(requestArr, msg, sizeof(filemsg));
-    strcpy(requestArr + sizeof(filemsg), fileString);
-
-    chan.cwrite(requestArr, sizeof(filemsg) + fileName.length() + 1);
-
-    char* response = chan.cread();
-
-    // Get packets of size 256MB and print to .csv file
-
-    freopen("./received/y1.csv", "w", stdout); //  prompt to write to new .csv file
-
-    __int64_t size = *(__int64_t*)response;
-
-
-    // find number of requests to make
-    int packetSize = 100;
-    int iters = size / packetSize;
-
-    // request multiple packets
-    int i;
-    for(i = 0; i <= iters; i++)
-    {
-        if(size % packetSize !=0 && i == iters)
-        {
-            msg = new filemsg(i*packetSize, size % packetSize);
-        }
-        else
-        {
-            msg = new filemsg(i*packetSize, packetSize);
-        }
-        memcpy(requestArr, msg, sizeof(filemsg)); // replaces msg with new one
-        chan.cwrite(requestArr, sizeof(filemsg) + fileName.length() + 1);
-        response = chan.cread();
-        cout << response;
-    }
-
-    fclose(stdout);
+    // fclose(stdout);
 
     // -------------- part 3 --------------------
 
-    // MESSAGE_TYPE message = NEWCHANNEL_MSG;
-    // chan.cwrite(&message, sizeof(MESSAGE_TYPE));
+    // MESSAGE_TYPE channelReq = NEWCHANNEL_MSG;
+    // chan.cwrite(&channelReq, sizeof(MESSAGE_TYPE));
     // char* message_response = chan.cread();
-    // FIFORequestChannel chan (message_response, FIFORequestChannel::CLIENT_SIDE);
+    // FIFORequestChannel chan2 (message_response, FIFORequestChannel::CLIENT_SIDE);
+
+    // double time = 0;
+    // double ecgVal;
+    // double unitTime = 0.004;
+
+    // freopen("./received/x1.csv", "w", stdout);
+
+    // for(double i = 0; time < 59.996; i++){
+    // // write the time to a file
+    //     time = unitTime*i;
+    //     cout << time << ",";
+
+    // // creates new data request
+    //     datamsg* ecg1 = new datamsg(1, time, 1);
+    //     datamsg* ecg2 = new datamsg(1, time, 2);
+
+    // // writes data request to channel chan
+    //     chan2.cwrite(ecg1, sizeof(datamsg));
+
+    // // stores data received from server in char*
+    //     char* ecg1_response = chan2.cread();
+    //     ecgVal = *(double*) ecg1_response;
+    //     cout << ecgVal << ",";
+
+    // // same steps for eg2
+    //     chan2.cwrite(ecg2, sizeof(datamsg));
+    //     char* ecg2_response = chan2.cread();
+    //     ecgVal = *(double*) ecg2_response;
+
+    //     cout << ecgVal << endl;
+    // }
+    // fclose(stdout);
 
     //---------------- part 4 ------------------
 
@@ -133,18 +164,9 @@ int main(int argc, char *argv[]){
 
 
 
-
-
-
-
-
-
-
-
-
-
     // closing the channel    
     MESSAGE_TYPE m = QUIT_MSG;
+    //chan2.cwrite (&m, sizeof (MESSAGE_TYPE));
     chan.cwrite (&m, sizeof (MESSAGE_TYPE));
 
    
